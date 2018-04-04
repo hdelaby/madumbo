71018d1faaf1ab50b878c053e9d6d3a42136839f
Hugo DELABY <hdelaby@e2r7p7.42.fr>
2018-04-04 16:55:05 +0200

    CSS and HTML parsing in progress

--- src/index.js
+++ src/index.js
@@ -1,21 +1,56 @@
 const jsdom = require('jsdom').JSDOM
 const fetch = require('node-fetch')
+const cssom = require('cssom')
+
+const extractSelectors = (cssRules) => {
+    for (let rule of cssRules) {
+        if (rule.selectorText) {
+            // console.log(rule.__starts)
+            const selectorGroup = rule.selectorText
+            const selectorList = selectorGroup.split(',')
+        }
+    }
+}
+
+const getCssFromFiles = async (urls) => {
+    for (let url of urls) {
+        try {
+            const httpResponse = await fetch(url)
+            const rawCss = await httpResponse.text()
+            const styles = cssom.parse(rawCss)
+            extractSelectors(styles.cssRules)
+        } catch(error) {
+            console.log('error: ', error)
+        }
+    }
+}
 
 const main = async () => {
-	try {
-		const response = await fetch('http://42.fr')
-		const html = await response.text()
-		const dom = new jsdom(html)
-		const links = dom.window.document.querySelectorAll('link')
-		links.forEach(elt => {
-			if (elt.rel === 'stylesheet') {
-				console.log(elt.href)
-			}
-		})
-	} catch (error) {
-		console.log('Error occured: ', error)
-		process.exit(1)
-	}
+    try {
+        const response = await fetch('http://42.fr')
+        const html = await response.text()
+        const dom = new jsdom(html)
+        const links = dom.window.document.querySelectorAll('*')
+        const linkUrls = []
+        const nodeNames = {}
+        const classNames = {}
+
+        links.forEach(elt => {
+            if (elt.rel === 'stylesheet') {
+                linkUrls.push(elt.href)
+            } else {
+                // nodeNames[elt.nodeName.toLowerCase()] = false
+                // elt.className.split(' ').forEach(cls => {
+                //     classNames[cls] = false
+                // })
+            }
+        })
+        // console.log(nodeNames, classNames)
+        getCssFromFiles(linkUrls)
+    } catch (error) {
+        console.log('Error occured: ', error)
+        process.exit(1)
+    }
 }
 
 main()
